7ab69654be824721376f349312385ad6bb7bcbd9
christophersansone <chris@voltage.io>
2018-02-05 13:35:54 -0500

    fixed attribute render spec

--- spec/lib/object_serializer_with_attribute_block_spec.rb
+++ spec/lib/object_serializer_with_attribute_block_spec.rb
@@ -9,12 +9,12 @@ describe FastJsonapi::ObjectSerializer do
       expect(serializable_hash[:data].length).to eq 2
       expect(serializable_hash[:data][0][:attributes].length).to eq 3
       expect(serializable_hash[:data][0][:attributes]).to have_key(:title_with_year)
-      expect(serializable_hash[:data][0][:attributes]).to eq "#{movie.name} (#{movie.release_year})"
+      expect(serializable_hash[:data][0][:attributes][:title_with_year]).to eq "#{movie.name} (#{movie.release_year})"
 
       serializable_hash = AMSMovieSerializerWithCustomMethod.new(movie_struct).serializable_hash
       expect(serializable_hash[:data][:attributes].length).to eq 3
       expect(serializable_hash[:data][:attributes]).to have_key(:title_with_year)
-      expect(serializable_hash[:data][0][:attributes]).to eq "#{movie.name} (#{movie.release_year})"
+      expect(serializable_hash[:data][0][:attributes][:title_with_year]).to eq "#{movie.name} (#{movie.release_year})"
     end
   end
 end
