5835c46e1c4976b1e513b5cb0d22c1660d1b76be
Dmitriy Ivliev <rootkit89@gmail.com>
2018-03-19 10:54:13 +0400

    add exception for missing id method

--- lib/fast_jsonapi/serialization_core.rb
+++ lib/fast_jsonapi/serialization_core.rb
@@ -4,6 +4,8 @@ require 'active_support/concern'
 require 'fast_jsonapi/multi_to_json'
 
 module FastJsonapi
+  MandatoryField = Class.new(StandardError)
+
   module SerializationCore
     extend ActiveSupport::Concern
 
@@ -75,8 +77,7 @@ module FastJsonapi
       def record_hash(record)
         if cached
           record_hash = Rails.cache.fetch(record.cache_key, expires_in: cache_length) do
-            id = record_id ? record.send(record_id) : record.id
-            temp_hash = id_hash(id, record_type) || { id: nil, type: record_type }
+            temp_hash = id_hash(id_from_record(record), record_type) || { id: nil, type: record_type }
             temp_hash[:attributes] = attributes_hash(record) if attributes_to_serialize.present?
             temp_hash[:relationships] = {}
             temp_hash[:relationships] = relationships_hash(record, cachable_relationships_to_serialize) if cachable_relationships_to_serialize.present?
@@ -85,12 +86,17 @@ module FastJsonapi
           record_hash[:relationships] = record_hash[:relationships].merge(relationships_hash(record, uncachable_relationships_to_serialize)) if uncachable_relationships_to_serialize.present?
           record_hash
         else
-          id = record_id ? record.send(record_id) : record.id
-          record_hash = id_hash(id, record_type) || { id: nil, type: record_type }
+          record_hash = id_hash(id_from_record(record), record_type) || { id: nil, type: record_type }
           record_hash[:attributes] = attributes_hash(record) if attributes_to_serialize.present?
           record_hash[:relationships] = relationships_hash(record) if relationships_to_serialize.present?
           record_hash
         end
+      end
+
+      def id_from_record(record)
+         return record.send(record_id) if record_id
+         raise MandatoryField, 'id is a mandatory field in the jsonapi spec' unless record.respond_to?(:id)
+         record.id
       end
 
       # Override #to_json for alternative implementation
