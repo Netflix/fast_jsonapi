270276d26426737b47060324ba6c9eaf03183c88
Ihor Dotsenko <ihor.dotsenko@rubygarage.org>
2018-03-19 10:24:27 +0200

    Fix bug with chaining rescues of FastJsonapi::MultiToJson::Result

---
+++ spec/lib/multi_to_json/result_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+module FastJsonapi
+  module MultiToJson
+    describe Result do
+      it 'supports chaining of rescues' do
+        expect do
+          Result.new(LoadError) do
+            require '1'
+          end.rescue do
+            require '2'
+          end.rescue do
+            require '3'
+          end.rescue do
+            '4'
+          end
+        end.not_to raise_error
+      end
+    end
+  end
+end
