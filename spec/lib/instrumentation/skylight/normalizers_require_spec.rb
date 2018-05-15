077817ecec1043ef39ef204fdb3c9329ceca9157
Les Fletcher <les.fletcher@gmail.com>
2018-05-15 08:14:46 -0700

    fix skylight normalizers issue

---
+++ spec/lib/instrumentation/skylight/normalizers_require_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe FastJsonapi::ObjectSerializer do
+
+  context 'instrument' do
+    context 'skylight' do
+      # skip for normal runs because this could alter some
+      # other test by insterting the instrumentation
+      xit 'make sure requiring skylight normalizers works' do
+        require 'fast_jsonapi/instrumentation/skylight'
+      end
+    end
+  end
+end
