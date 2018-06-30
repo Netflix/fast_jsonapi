fe5694bf0b286d57ffd6c115adf623291a74fe34
Kyle Reeves <kyreeves@icloud.com>
2018-06-30 17:59:42 -0500

    all tests are passing, but still need to write tests for relationship class

--- spec/shared/examples/object_serializer_class_methods_examples.rb
+++ spec/shared/examples/object_serializer_class_methods_examples.rb
@@ -1,10 +1,10 @@
 RSpec.shared_examples 'returning correct relationship hash' do |serializer, id_method_name, record_type|
   it 'returns correct relationship hash' do
     expect(relationship).to be_instance_of(FastJsonapi::Relationship)
-    expect(relationship.keys).to all(be_instance_of(Symbol))
-    expect(relationship[:serializer]).to be serializer
-    expect(relationship[:id_method_name]).to be id_method_name
-    expect(relationship[:record_type]).to be record_type
+    # expect(relationship.keys).to all(be_instance_of(Symbol))
+    expect(relationship.serializer).to be serializer
+    expect(relationship.id_method_name).to be id_method_name
+    expect(relationship.record_type).to be record_type
   end
 end
 
