01477e9c5b4d09b0a4d92034fd55a4c54cf0c2c9
Kyle Reeves <kyreeves@icloud.com>
2018-07-03 15:18:52 -0500

    fix relationship id_hash method

--- lib/fast_jsonapi/relationship.rb
+++ lib/fast_jsonapi/relationship.rb
@@ -77,11 +77,11 @@ module FastJsonapi
       id_hash(ids, record_type) # ids variable is just a single id here
     end
 
-    def id_hash(id, associated_record_type, default_return=false)
+    def id_hash(id, record_type, default_return=false)
       if id.present?
         { id: id.to_s, type: record_type }
       else
-        default_return ? { id: nil, type: associated_record_type } : nil
+        default_return ? { id: nil, type: record_type } : nil
       end
     end
 
