af38b30179b1ac842aec2e88c8aca5a61f0f2411
Kyle Reeves <kyreeves@icloud.com>
2018-07-03 15:58:22 -0500

    remove options param from Link class

--- lib/fast_jsonapi/link.rb
+++ lib/fast_jsonapi/link.rb
@@ -1,28 +1,17 @@
 module FastJsonapi
   class Link
-    attr_reader :key, :method, :conditional_proc
+    attr_reader :key, :method
 
-    def initialize(key:, method:, options: {})
+    def initialize(key:, method:)
       @key = key
       @method = method
-      @conditional_proc = options[:if]
     end
 
     def serialize(record, serialization_params, output_hash)
-      if include_link?(record, serialization_params)
-        output_hash[key] = if method.is_a?(Proc)
-          method.arity == 1 ? method.call(record) : method.call(record, serialization_params)
-        else
-          record.public_send(method)
-        end
-      end
-    end
-
-    def include_link?(record, serialization_params)
-      if conditional_proc.present?
-        conditional_proc.call(record, serialization_params)
+      output_hash[key] = if method.is_a?(Proc)
+        method.arity == 1 ? method.call(record) : method.call(record, serialization_params)
       else
-        true
+        record.public_send(method)
       end
     end
   end
