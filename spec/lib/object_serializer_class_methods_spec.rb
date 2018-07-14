e05193fb5e6de1c901f95bfce76523fd5de4b0ba
Erol <erol.fornoles@gmail.com>
2018-07-14 17:11:24 +0800

    Add spec for proc methods with optional arguments

--- spec/lib/object_serializer_class_methods_spec.rb
+++ spec/lib/object_serializer_class_methods_spec.rb
@@ -235,6 +235,7 @@ describe FastJsonapi::ObjectSerializer do
       before do
         movie.release_year = 2008
         MovieSerializer.attribute :released_in_year, &:release_year
+        MovieSerializer.attribute :name, &:local_name
       end
 
       after do
@@ -242,7 +243,7 @@ describe FastJsonapi::ObjectSerializer do
       end
 
       it 'returns correct hash when serializable_hash is called' do
-        expect(serializable_hash[:data][:attributes][:name]).to eq movie.name
+        expect(serializable_hash[:data][:attributes][:name]).to eq "english #{movie.name}"
         expect(serializable_hash[:data][:attributes][:released_in_year]).to eq movie.release_year
       end
     end
